5e24ba7737ac87b4312bf5d4998a8ecd6dfeb963
hk0308 <148302687+hk0308@users.noreply.github.com>
2024-01-11 12:23:05 +0900

    menu作成中

---
+++ src/menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sweets メニュー</title>
+</head>
+<body>
+    <h1>スイーツメニュー</h1>
+    <button onclick="location.href='sweet_list.php'">一覧</button>
+    <button onclick="location.href='sweet_insert.php'">登録</button>
+    <button onclick="location.href='sweet_update.php'">更新</button>
+    <button onclick="location.href='sweet_delite.php'">削除</button>
+</body>
+</html>
